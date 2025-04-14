2612087bb50735f99b5beef22b9529163d347e56
Mox20 <161859107+Mox20@users.noreply.github.com>
2025-04-15 01:34:27 +0300

    Initial commit

--- furry_tuesday_bot.py
+++ furry_tuesday_bot.py
@@ -2,20 +2,17 @@ import telebot
 import schedule
 import time
 import requests
+from requests.auth import HTTPBasicAuth
 
-
-TOKEN = '7684292215:AAGDjIpq5hFWyrjeK978dX3NqfnLXjR_Gu4'
-
-E621_API_KEY = 'z7mqRqeDXurW2VvJf68HgiEC'
-USERNAME = "MaxSaucer"
-
-PROXY =  "http://itmarketbg:jZB4aU7f5B@77.90.178.235:50100"
+TOKEN = 'MY_TOKEN'
+E621_API_KEY = 'API_KEY'
+USERNAME = "MY_NAME"
 
 # Инициализация бота
 bot = telebot.TeleBot(TOKEN)
 
 # Переменная для хранения chat_id
-chat_id = None
+chat_id = 712363460 # None
 
 # Функция для обработки команды /start
 @bot.message_handler(commands=['start'])
@@ -33,31 +30,31 @@ def porn(nessage):
 # Функция для получения самой популярной картинки с e621.net
 def get_most_image_data():
     url = 'https://e621.net/posts.json'
+
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0'
+        'User-Agent': 'FurryTuesdayBot/1.0 (by @F@maksaucer)',
+        'Host': 'e621.net'
     }
 
     params = {
         'limit': 1,
-        'tags':{
-            'order':'score',
-            'date':'day'
-        }
+        'tags': 'order:score date:day'
     }
 
-    proxies = {
-        'http': PROXY,
-        'https': PROXY,
-    }
     try:
-        response = requests.get(url, headers=headers, params=params, proxies=proxies)
+        response = requests.get(
+            url, 
+            headers=headers,
+            params=params,
+            auth=HTTPBasicAuth(USERNAME, E621_API_KEY),  # Авторизация
+            timeout=10
+        )
         response.raise_for_status()
-        post_data = response.json()
-        if post_data['posts']:
-            return post_data
+        return response.json()
     except requests.exceptions.RequestException as e:
         print(f'Request failed: {e}')
     return None
+
 
 # Функция для отправки картинки
 def send_image():
